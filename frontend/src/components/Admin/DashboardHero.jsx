f53883e7ea23913e15e2e32265202e6ff4dfd0c6
stphenson <anonuevostphenson@gmail.com>
2024-05-13 15:16:49 +0800

    changes in hero dashboard

--- frontend/src/components/Admin/DashboardHero.jsx
+++ frontend/src/components/Admin/DashboardHero.jsx
@@ -10,6 +10,7 @@ import { Button } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { FiShoppingBag } from 'react-icons/fi';
 import { HiOutlineReceiptRefund } from 'react-icons/hi';
+import {IoBagHandle, IoPieChart, IoPeople, IoCart} from 'react-icons/io5' 
 // import { LuUser2 } from 'react-icons/lu';
 
 const DashboardHero = () => {
@@ -95,6 +96,10 @@ const DashboardHero = () => {
 
   const row = [];
 
+  const BoxWrapper = ({children}) =>{
+    return <div className="bg-white rounded-sm p-4 flex-1 border border-gray-200 flex items-center">{children}</div>
+  }
+
   orders &&
     orders.forEach((item) => {
       row.push({
@@ -112,79 +117,40 @@ const DashboardHero = () => {
     <div className="w-full p-8">
       <h3 className="text-[22px] font-Poppins pb-2">Overview</h3>
       <div className="w-full block 800px:flex items-center justify-between">
-        {/* Total Earnings */}
-        <div className="w-[80%] 800px:ml-0 ml-[40px] mb-4 800px:w-[18%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
-          <div className="flex items-center">
-            <AiOutlineMoneyCollect size={30} className="mr-2" fill="#171203" />
-            <h3
-              className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#534723]`}
-            >
-              Total Earnings
-            </h3>
+        {/* Total Sales */}
+         <BoxWrapper>
+          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+            <IoBagHandle className='text-2xl text-white'/>
           </div>
-          <h5 className="!text-[13px] text-[#958247]">(Less shipping cost)</h5>
-          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">
-            ₱ {availableBalance.toFixed(2)}
-          </h5>
-        </div>
-
-        {/* All Orders */}
-        <div className="w-[80%] 800px:ml-0 ml-[40px] mb-4 800px:w-[18%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
-          <div className="flex items-center">
-            <MdBorderClear size={30} className="mr-2" fill="#171203" />
-            <h3
-              className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#534723]`}
-            >
-              All Orders
-            </h3>
+          <div className="pl-4">
+            <span className="text-sm text-gray-500 font-light">Total Sales</span>
+            <div className="flex items-center">
+              <strong className='text-xl text-gray-700 font-semibold'>₱ {availableBalance.toFixed(2)}</strong>
+            </div>
           </div>
-          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">
-            {' '}
-            {orders && orders.length}
-          </h5>
-          <Link to="/dashboard-orders">
-            <h5 className="pt-4 pl-2 text-[#171203]">View Orders</h5>
-          </Link>
-        </div>
+        </BoxWrapper>
 
-        {/* All Refunds */}
-        <div className="w-[80%] 800px:ml-0 ml-[40px] mb-4 800px:w-[18%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
-          <div className="flex items-center">
-            <HiOutlineReceiptRefund size={30} className="mr-2" />
-            <h3
-              className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#534723]`}
-            >
-              All Refunds
-            </h3>
-          </div>
-          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">
-            {refundApprovedCount}
-          </h5>
-          <Link to="/dashboard-refunds">
-            <h5 className="pt-4 pl-2 text-[#171203]">View Refunds</h5>
-          </Link>
-        </div>
+        {/* TODO: Add total Expenses */}
 
-        {/* All Products */}
-        <div className="w-[80%] 800px:ml-0 ml-[40px] mb-4 800px:w-[18%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
-          <div className="flex items-center">
-            <FiShoppingBag size={27} className="mr-2" />
-            <h3
-              className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#534723]`}
-            >
-              All Products
-            </h3>
-          </div>
-          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">
-            {products && products.length}
-          </h5>
-          <Link to="/dashboard-products">
-            <h5 className="pt-4 pl-2 text-[#171203]">View Products</h5>
-          </Link>
-        </div>
+        {/* TODO: Add total Customers */}
 
-        {/* All User
-        <div className="w-[80%] 800px:ml-0 ml-[40px] mb-4 800px:w-[18%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
+        {/* Total Orders */}
+        <Link to="/dashboard-orders">
+          <BoxWrapper>
+              <div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
+                  <IoCart className="text-2xl text-white" />
+              </div>
+              <div className="pl-4">
+                <span className="text-sm text-gray-500 font-light">Total Orders</span>
+                <div className="flex items-center">
+                  <strong className="text-xl text-gray-700 font-semibold">{orders && orders.length}</strong>
+                </div>
+              </div>
+          </BoxWrapper>
+        </Link>
+        
+        {/* all users */}
+        {/* <div className="w-[80%] 800px:ml-0 ml-[40px] mb-4 800px:w-[18%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
           <div className="flex items-center">
             <LuUser2 size={30} className="mr-2" />
             <h3
@@ -202,6 +168,10 @@ const DashboardHero = () => {
         </div> */}
       </div>
       <br />
+
+      {/* TODO: Add Analytics Bar Chart */}
+
+      {/* Section for Latest Orders */}
       <h3 className="text-[22px] font-Poppins pb-2 text-[171203]">
         Latest Orders
       </h3>
