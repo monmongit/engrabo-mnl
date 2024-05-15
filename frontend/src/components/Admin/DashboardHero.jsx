d1136d3a4c5500ffbff8395f1535a0d358287a32
kambal <Kambal_account@Aprils-MacBook-Air.local>
2024-05-16 00:29:24 +0800

    compute total expenses, refactor functions in dashboard, create analytics dashboard for monthly expenses-sales-orders

--- frontend/src/components/Admin/DashboardHero.jsx
+++ frontend/src/components/Admin/DashboardHero.jsx
@@ -11,7 +11,7 @@ import { getAllUsers } from '../../redux/action/user';
 import { Button } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import {IoBagHandle, IoPieChart, IoPeople, IoCart} from 'react-icons/io5' 
-// import { LuUser2 } from 'react-icons/lu';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const DashboardHero = () => {
   const dispatch = useDispatch();
@@ -21,6 +21,7 @@ const DashboardHero = () => {
   const { products } = useSelector((state) => state.products);
   const [deliveredOrder, setDeliveredOrder] = useState(null);
   
+  console.log("orders: ",orders)
   console.log(admin);
   useEffect(() => {
     if (admin && admin._id) {
@@ -36,99 +37,15 @@ const DashboardHero = () => {
     setDeliveredOrder(orderData);
   }, [orders]);
 
-  const totalEarningWithoutTax =
-    deliveredOrder &&
-    deliveredOrder.reduce((acc, item) => acc + item.totalPrice, 0);
+  // Call our Computation Functions, Order Dashboard and Analytics 
+  const availableBalance = computeTotalSales(deliveredOrder);
+  const totalExpenses = computeTotalExpenses(products);
+  const {columns, row} = ordersDashboard(orders)
+  const displayAnalytics = transactionChart(orders, products)
 
-  const serviceCharge = totalEarningWithoutTax
-    ? totalEarningWithoutTax * 0.1
-    : 0;
-
-  const availableBalance = totalEarningWithoutTax
-    ? totalEarningWithoutTax - serviceCharge
-    : 0;
-
-  // computes the total expenses
-   const ComputetotalExpenses = () => {
-    if (!products || products.length === 0) {
-      return 0;
-    }
-
-    // Iterate over each product and calculate its total cost based on stock and original price
-    const totalExpenses = products.reduce((total, product) => {
-      const productExpense = product.stock * product.originalPrice;
-      return total + productExpense;
-    }, 0);
-
-    return totalExpenses;
- }
-
- const totalExpenses = ComputetotalExpenses();
-
-  const columns = [
-    { field: 'id', headerName: 'Order ID', minWidth: 150, flex: 0.7 },
-    {
-      field: 'status',
-      headerName: 'Status',
-      minWidth: 130,
-      flex: 0.7,
-      cellClassName: (params) => {
-        return params.row.status === 'Delivered' ? 'greenColor' : 'redColor';
-      },
-    },
-    {
-      field: 'itemsQty',
-      headerName: 'Items Qty',
-      type: 'number',
-      minWidth: 130,
-      flex: 0.7,
-    },
-
-    {
-      field: 'total',
-      headerName: 'Total',
-      type: 'number',
-      minWidth: 130,
-      flex: 0.8,
-    },
-
-    {
-      field: ' ',
-      flex: 1,
-      minWidth: 150,
-      headerName: '',
-      type: 'number',
-      sortable: false,
-      renderCell: (params) => {
-        return (
-          <>
-            <Link to={`/order/${params.id}`}>
-              <Button>
-                <AiOutlineArrowRight size={20} />
-              </Button>
-            </Link>
-          </>
-        );
-      },
-    },
-  ];
-
-  const row = [];
-
-  const BoxWrapper = ({children}) =>{
-    return <div className="bg-white rounded-sm p-4 flex-1 border border-gray-200 flex items-center">{children}</div>
-  }
-
-  orders &&
-    orders.forEach((item) => {
-      row.push({
-        id: item._id,
-        itemsQty: item.cart.reduce((acc, item) => acc + item.qty, 0),
-        total: '₱ ' + item.totalPrice,
-        status: item.status,
-      });
-    });
-
+  // css stying custom 
+  const BoxWrapper = ({children}) =>{return <div className="bg-white rounded-sm p-4 flex-1 border border-gray-200 flex items-center">{children}</div>}
+  
   return (
     <div className="w-full p-8">
       <h3 className="text-[22px] font-Poppins pb-2">Overview</h3>
@@ -192,8 +109,10 @@ const DashboardHero = () => {
       </div>
       
       <br />
-
-      {/* TODO: Add Analytics Bar Chart */}
+      {/* transactions */}
+      <div className="transaction-analytic">
+        {displayAnalytics}
+      </div>
 
       {/* Section for Latest Orders */}
       <h3 className="text-[22px] font-Poppins pb-2 text-[171203]">
@@ -211,5 +130,210 @@ const DashboardHero = () => {
     </div>
   );
 };
+
+
+// FUNCTION DEFINITIONS
+const computeTotalSales = (deliveredOrder) => {
+  const totalEarningWithoutTax =
+    deliveredOrder &&
+    deliveredOrder.reduce((acc, item) => acc + item.totalPrice, 0);
+
+  const serviceCharge = totalEarningWithoutTax
+    ? totalEarningWithoutTax * 0.1
+    : 0;
+
+  const availableBalance = totalEarningWithoutTax
+    ? totalEarningWithoutTax - serviceCharge
+    : 0;
+
+  return availableBalance;
+}
+const computeTotalExpenses = (products) => {
+  if (!products || products.length === 0) {
+    return 0;
+  }
+
+  // Iterate over each product and calculate its total cost based on stock and gross price
+  const totalExpenses = products.reduce((total, product) => {
+    const productExpense = product.stock * product.grossPrice;
+    return total + productExpense;
+  }, 0);
+
+  return totalExpenses;
+}
+
+const ordersDashboard = (orders) => {
+
+  const columns = [
+    { field: 'id', headerName: 'Order ID', minWidth: 150, flex: 0.7 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.row.status === 'Delivered' ? 'greenColor' : 'redColor';
+      },
+    },
+    {
+      field: 'itemsQty',
+      headerName: 'Items Qty',
+      type: 'number',
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: 'total',
+      headerName: 'Total',
+      type: 'number',
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: ' ',
+      flex: 1,
+      minWidth: 150,
+      headerName: '',
+      type: 'number',
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.cart.reduce((acc, item) => acc + item.qty, 0),
+        total: '₱ ' + item.totalPrice,
+        status: item.status,
+      });
+    });
+
+  return {columns, row}
+}
+
+const transactionChart = (orders,products) => {
+  // check if ordrers 
+  if(!orders || !orders.length ){
+    console.log("orders data not available")
+  } 
+  // Initialize monthly sales object with zeros for all months
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const monthlySales = {};
+  for (let month = 0; month < 12; month++) {
+    const key = `${currentYear}-${month}`;
+    monthlySales[key] = {
+      month: month,
+      year: currentYear,
+      sales: 0,
+      orders: 0
+    };
+  }
+
+  // Group orders by month
+  orders.forEach(order => { 
+      const paidAt = new Date(order.paidAt);
+      const month = paidAt.getMonth();
+      const year = paidAt.getFullYear();
+      const key = `${year}-${month}`;
+      monthlySales[key].orders++;
+      if (order.status === "Delivered") {
+      monthlySales[key].sales += order.totalPrice;
+    }
+  });
+
+  console.log("Product for computing expenses: ", products)
+  // Group expenses by month
+  const monthlyExpenses = {};
+  products.forEach(product => {
+    const createdAt = new Date(product.createAt);
+    const month = createdAt.getMonth();
+    const year = createdAt.getFullYear();
+    const key = `${year}-${month}`;
+    const productExpenses = product.stock * product.grossPrice;
+    if (!monthlyExpenses[key]) {
+      monthlyExpenses[key] = {
+        month: month,
+        year: year,
+        expenses: productExpenses
+      };
+    } else {
+      monthlyExpenses[key].expenses += productExpenses;
+    }
+  });
+
+  console.log("monthly expenses: ", monthlyExpenses)
+
+  // Convert object to array of monthly sales with sales formatted to two decimal places
+  const formattedData = Object.values(monthlySales).map(item => ({
+    month: new Date(item.year, item.month, 1).toLocaleString('en-US', { month: 'long' }),
+    sales: item.sales.toFixed(2), // Format sales to two decimal places
+    orders: item.orders,
+    expenses: monthlyExpenses[`${item.year}-${item.month}`] ? monthlyExpenses[`${item.year}-${item.month}`].expenses.toFixed(2) : 0
+  }));
+
+  console.log("Formatted Data: ",formattedData);
+
+
+  const data = formattedData;
+  return (
+    <div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
+      <div className='flex justify-between items-center'>
+        <div className='align-middle'>
+          <strong className="text-gray-700 font-medium">Transactions</strong>
+        </div>
+        <div className="mb-2 align-middle">
+          {/* <button className="bg-blue-400 hover:bg-blue-700 text-white font-medium py-1 px-2 rounded" onClick={downloadCsv}>
+            Download CSV
+          </button> */}
+        </div>
+      </div>
+      <div className="mt-3 w-full flex-1 text-xs">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={600}
+            data={data}
+            margin={{
+              top: 20,
+              right: 10,
+              left: -10,
+              bottom: 0
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+            <XAxis dataKey="month" />
+            <YAxis tickCount={100} tick={{ fontSize: 12, fontFamily: 'Arial', dy: 5 }} />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="sales" fill="#0ea5e9" />
+            <Bar dataKey="expenses" fill="#ea580c" />
+            <Bar dataKey="orders" fill="#82ca9d" /> 
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+}
+
+// To do: Y axix of analytics, Use ApexCharts instead, Functionality to download the reports
+
 
 export default DashboardHero;
