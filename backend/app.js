5fd30cd86d258b9979485ad56ffc2c3eae4aff60
stphenson <anonuevostphenson@gmail.com>
2024-05-22 02:20:30 +0800

    app js updated for frontend path

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://engrabo-mnl-frnt.vercel.app/',
     credentials: true,
   })
 );
