bbd996e9ad77d21a56af3bafe2ed1a25fa9d01f5
stphenson <anonuevostphenson@gmail.com>
2024-05-22 10:22:57 +0800

    changes in app js and cors origin

--- backend/app.js
+++ backend/app.js
@@ -1,54 +1,54 @@
-const express = require('express');
-const ErrorHandler = require('./middleware/error');
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
 const app = express();
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path')
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://engrabo-mnl-frnt.vercel.app/',
+    origin: "https://engrabo-mnl-frnt.vercel.app",
     credentials: true,
   })
 );
-app.use("/", express.static(path.join(__dirname, "uploads")))
-app.use("/test", (req,res) => {
-  res.send("Please Work")
-})
+app.use("/", express.static(path.join(__dirname, "uploads")));
+app.use("/test", (req, res) => {
+  res.send("Please Work");
+});
 
 app.use(cookieParser());
 
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // config
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-  require('dotenv').config({
-    path: 'config/.env',
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
   });
 }
 
 // Controller Routes
-const user = require('./controller/user');
-const order = require('./controller/order');
-const admin = require('./controller/admin');
-const product = require('./controller/product');
-const event = require('./controller/event');
-const coupon = require('./controller/couponCode');
-const category = require('./controller/category');
-const conversation = require('./controller/conversation');
-const message = require('./controller/message');
+const user = require("./controller/user");
+const order = require("./controller/order");
+const admin = require("./controller/admin");
+const product = require("./controller/product");
+const event = require("./controller/event");
+const coupon = require("./controller/couponCode");
+const category = require("./controller/category");
+const conversation = require("./controller/conversation");
+const message = require("./controller/message");
 
-app.use('/api/v2/user', user);
-app.use('/api/v2/conversation', conversation);
-app.use('/api/v2/message', message);
-app.use('/api/v2/order', order);
-app.use('/api/v2/admin', admin);
-app.use('/api/v2/product', product);
-app.use('/api/v2/event', event);
-app.use('/api/v2/coupon', coupon);
-app.use('/api/v2/category', category);
+app.use("/api/v2/user", user);
+app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
+app.use("/api/v2/order", order);
+app.use("/api/v2/admin", admin);
+app.use("/api/v2/product", product);
+app.use("/api/v2/event", event);
+app.use("/api/v2/coupon", coupon);
+app.use("/api/v2/category", category);
 
 // ErrorHandling
 app.use(ErrorHandler);
