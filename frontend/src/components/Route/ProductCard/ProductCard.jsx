984590f10addd037cebdeb55de45c120bee1f505
stphenson <anonuevostphenson@gmail.com>
2024-05-27 00:50:38 +0800

    changes the ui for product card in all pages

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -37,7 +37,7 @@ const ProductCard = ({ data, isEvent }) => {
   const removeFromWishlistHandler = (data) => {
     setClick(!click);
     dispatch(removeFromWishlist(data));
-    toast.error(`${data.name} removed to wishlist successfully!`);
+    toast.error(`${data.name} removed from wishlist successfully!`);
   };
 
   const addToWishlistHandler = (data) => {
@@ -49,7 +49,7 @@ const ProductCard = ({ data, isEvent }) => {
   const addToCartHandler = (id) => {
     const isItemExists = cart && cart.find((i) => i._id === id);
     if (isItemExists) {
-      toast.error(`${data.name} already in a cart!`);
+      toast.error(`${data.name} already in cart!`);
     } else {
       if (data.stock < 1) {
         toast.error(
@@ -65,107 +65,105 @@ const ProductCard = ({ data, isEvent }) => {
 
   return (
     <>
-      <div className="w-full h-[370px]  bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
-        <Link
-          to={`${
-            isEvent === true
-              ? `/product/${data._id}?isEvent=true`
-              : `/product/${data._id}`
-          }`}
-        >
-          <img
-            src={`${data.images && data.images[0]?.url} `}
-            alt=""
-            className="w-5/6 h-44 object-contain"
-          />
-        </Link>
+      <div className="w-60 p-2 bg-white rounded-xl transform transition-all hover:translate-y-2 duration-300 shadow-lg hover:shadow-2xl mt-4 mb-4 lg:mt-0">
+        <div className="w-full h-40 overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
+          <Link
+            to={`${
+              isEvent === true
+                ? `/product/${data._id}?isEvent=true`
+                : `/product/${data._id}`
+            }`}
+          >
+            <img
+              src={data.images && data.images[0]?.url}
+              alt={data.name}
+              className="w-full h-full object-cover object-center"
+            />
+          </Link>
+        </div>
 
-        <Link
-          to={`${
-            isEvent === true
-              ? `/product/${data._id}?isEvent=true`
-              : `/product/${data._id}`
-          }`}
-        >
-          {/* Name of Product */}
-          <h4 className="pb-1 pt-4 font-[500]">
-            {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
-          </h4>
+        {/* product information */}
+        <div className="p-2">
+          {/* name and price */}
+          <Link
+            to={`${
+              isEvent === true
+                ? `/product/${data._id}?isEvent=true`
+                : `/product/${data._id}`
+            }`}
+          >
+            <h2 className="font-bold text-lg mb-2">
+              {data.name.length > 40
+                ? data.name.slice(0, 40) + "..."
+                : data.name}
+            </h2>
+          </Link>
+          <div className="flex gap-1">
+            <span className="text-xl font-semibold">
+              ₱{data.discountPrice ? data.discountPrice : data.originalPrice}
+            </span>
+            {data.discountPrice && (
+              <div className="flex items-center gap-2">
+                <span className="text-sm line-through opacity-75">
+                  ₱{data.originalPrice}
+                </span>
+                <span className="font bold text-sm p-1 bg-yellow-300">
+                  Save{" "}
+                  {Math.round(
+                    (1 - data.discountPrice / data.originalPrice) * 100
+                  )}
+                  %
+                </span>
+              </div>
+            )}
+          </div>
 
-          {/* Product Description */}
-          <p className="text-justify pt-1 pb-2 text-[#534723]">
+          <div className="flex items-center mt-2 gap-1">
+            <Ratings rating={data?.ratings} />
+            <p className="font-bold text-xs text-gray-700">
+              {data.ratings ? data?.ratings : "0.0"}
+            </p>
+          </div>
+
+          <p className="text-sm text-gray-600 mt-2 mb-2">
             {data.description.length > 40
               ? data.description.slice(0, 40) + "..."
               : data.description}
           </p>
+        </div>
 
-          {/* Rating of Product */}
-          <div className="flex pb-2 pt-1">
-            <Ratings rating={data?.ratings} />
-          </div>
-
-          {/* Price and Sold of Product */}
-          <div className="py-2 flex items-center justify-between">
-            {/* Price of Product */}
-            <div className="flex">
-              <h5 className={`${styles.productDiscountPrice}`}>
-                ₱{data.discountPrice ? data.discountPrice : data.originalPrice}
-              </h5>
-              <h4 className={`${styles.price}`}>
-                {data.discountPrice ? "₱ " + data.originalPrice : null}
-              </h4>
-            </div>
-
-            {/* Sold of Product */}
-            <span className="font-[400] text-[17px] text-[#b19b56]">
-              {data?.sold_out} sold
-            </span>
-          </div>
-        </Link>
-
-        {/* Option of Product */}
-        <div>
+        {/* buttons */}
+        <div className="flex items-center justify-center gap-2 mb-3">
+          <button
+            className="px-3 py-1 rounded-lg bg-gray-300 hover:bg-blue-400"
+            onClick={() => setOpen(!open)}
+          >
+            <AiOutlineEye className="w-6" />
+          </button>
           {click ? (
-            <AiFillHeart
-              size={22}
-              className="cursor-pointer absolute right-2 top-5"
+            <button
+              className="px-3 py-1 rounded-lg bg-gray-300 hover:bg-yellow-400 hover"
               onClick={() => removeFromWishlistHandler(data)}
-              color={click ? "#171203" : "#171203"}
-              title="Removed from Wishlist"
-            />
+            >
+              <AiFillHeart className="w-6" color="white" />
+            </button>
           ) : (
-            <AiOutlineHeart
-              size={22}
-              className="cursor-pointer absolute right-2 top-5"
+            <button
+              className="px-3 py-1 rounded-lg bg-gray-300 hover:bg-yellow-400"
               onClick={() => addToWishlistHandler(data)}
-              color={click ? "#171203" : "#171203"}
-              title="Added to Wishlist"
-            />
+            >
+              <AiOutlineHeart className="w-6" />
+            </button>
           )}
-          <AiOutlineEye
-            size={22}
-            className="cursor-pointer absolute right-2 top-14"
-            onClick={() => setOpen(!open)}
-            color="#171203"
-            title="Quick View"
-          />
-          <AiOutlineShoppingCart
-            size={25}
-            className="cursor-pointer absolute right-2 top-24"
+          <button
+            className="flex items-center gap-2 px-3 py-1 rounded-lg bg-gray-300 hover:bg-green-400"
             onClick={() => addToCartHandler(data._id)}
-            color="#171203"
-            title="Add to Cart"
-          />
-          <FaRegEdit
-            size={23}
-            className="cursor-pointer absolute right-2 top-36"
-            onClick={() => setOpen(!open)}
-            color="#171203"
-            title="Edit Product"
-          />
-          {open ? <ProductDetailsCard setOpen={setOpen} data={data} /> : null}
+          >
+            <AiOutlineShoppingCart />
+          </button>
         </div>
       </div>
+      {open ? <ProductDetailsCard setOpen={setOpen} data={data} /> : null}
     </>
   );
 };
--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -35,9 +35,8 @@ const ProductsPage = () => {
           <br />
           <br />
           <div className={`${styles.section}`}>
-            <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mx-auto mb-12">
-              {data &&
-                data.map((i, index) => <ProductCard data={i} key={index} />)}
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 justify-items-center">
+                {data.map((i, index) => <ProductCard data={i} key={index} />)}
             </div>
             {data && data.length === 0 ? (
               <h1 className="text-center w-full pb-[110px] text-[20px]">
