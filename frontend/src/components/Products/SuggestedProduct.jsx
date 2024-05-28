76936b7a8a413829af7e94efe593baeed1eea07f
stphenson <anonuevostphenson@gmail.com>
2024-05-28 09:44:57 +0800

    fix ui in every page that has cards

--- frontend/src/components/Products/SuggestedProduct.jsx
+++ frontend/src/components/Products/SuggestedProduct.jsx
@@ -23,7 +23,7 @@ const SuggestedProduct = ({ data }) => {
           >
             Related Product
           </h2>
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 justify-items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 justify-items-center">
             {productData &&
               productData.map((i, index) => (
                 <ProductCard data={i} key={index} />
