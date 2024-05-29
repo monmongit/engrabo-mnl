0c5b42c7059e36ee397eb05e0cd071467d14bb3a
Chie0011 <delapenaarchie604@gmail.com>
2024-05-30 06:32:44 +0800

    merge admin UI changes 3

--- frontend/src/pages/Admin/AdminAllRefunds.jsx
+++ frontend/src/pages/Admin/AdminAllRefunds.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import DashboardHeader from '../../components/Admin/Layout/DashboardHeader';
-import DashboardSideBar from '../../components/Admin/Layout/DashboardSideBar';
-import AllRefundOrders from '../../components/Admin/AllRefundOrders';
+import React from "react";
+import DashboardHeader from "../../components/Admin/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/Admin/Layout/DashboardSideBar";
+import AllRefundOrders from "../../components/Admin/AllRefundOrders";
 
 const AdminAllRefunds = () => {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <DashboardHeader />
-      <div className="flex justify-between w-full">
+      <div className="flex flex-1 flex-col-reverse md:flex-row w-full">
         <div className="w-[80px] 800px:w-[330px]">
           <DashboardSideBar active={7} />
         </div>
-        <div className="w-full justify-center flex">
+        <div className="flex-1 overflow-x-auto mx-4">
           <AllRefundOrders />
         </div>
       </div>
--- frontend/src/pages/Admin/AdminSettingsPage.jsx
+++ frontend/src/pages/Admin/AdminSettingsPage.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import AdminSettings from '../../components/Admin/AdminSettings';
-import DashboardHeader from '../../components/Admin/Layout/DashboardHeader';
-import DashboardSideBar from '../../components/Admin/Layout/DashboardSideBar';
+import React, { useState, useEffect } from "react";
+import AdminSettings from "../../components/Admin/AdminSettings";
+import DashboardHeader from "../../components/Admin/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/Admin/Layout/DashboardSideBar";
 
 const ShopSettingsPage = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <DashboardHeader />
-      <div className="flex items-start justify-between w-full">
-        <div className="w-[80px] 800px:w-[330px]">
-          <DashboardSideBar active={11} />
+      <div
+        className={`flex ${isMobile ? "flex-col-reverse" : "flex-row"} w-full`}
+      >
+        <div
+          className={`w-[80px] 800px:w-[330px] ${
+            isMobile ? "" : "fixed left-0 h-full"
+          }`}
+        >
+          <DashboardSideBar active={13} />
         </div>
-        <AdminSettings />
+        <div
+          className={`flex-1 px-4 overflow-x-auto mx-2 mt-3 ${
+            isMobile ? "" : "ml-[330px]"
+          }`}
+        >
+          <AdminSettings />
+        </div>
       </div>
     </div>
   );
