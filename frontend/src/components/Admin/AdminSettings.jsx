cf0def61eaa9916b87daebdf74db9e5e12ad8b97
Chie0011 <delapenaarchie604@gmail.com>
2024-05-30 05:55:09 +0800

    merge updated admin side ui

--- frontend/src/components/Admin/AdminSettings.jsx
+++ frontend/src/components/Admin/AdminSettings.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { server } from '../../server';
-import { AiOutlineCamera } from 'react-icons/ai';
-import styles from '../../styles/style';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { loadAdmin } from '../../redux/action/user';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { server } from "../../server";
+import { AiOutlineCamera } from "react-icons/ai";
+import styles from "../../styles/style";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { loadAdmin } from "../../redux/action/user";
+import Button from "@mui/material/Button";
 
 const AdminSettings = () => {
   const { admin } = useSelector((state) => state.admin);
   const [avatar, setAvatar] = useState();
   const [name, setName] = useState(admin && admin.name);
   const [description, setDescription] = useState(
-    admin && admin.description ? admin.description : ''
+    admin && admin.description ? admin.description : ""
   );
   const [address, setAddress] = useState(admin && admin.address);
   const [phoneNumber, setPhoneNumber] = useState(admin && admin.phoneNumber);
@@ -36,7 +37,7 @@ const AdminSettings = () => {
           )
           .then((res) => {
             dispatch(loadAdmin());
-            toast.success('Avatar updated successfully!');
+            toast.success("Avatar updated successfully!");
           })
           .catch((error) => {
             toast.error(error.response.data.message);
@@ -63,7 +64,7 @@ const AdminSettings = () => {
         { withCredentials: true }
       )
       .then((res) => {
-        toast.success('Shop info updated succesfully!');
+        toast.success("Shop info updated succesfully!");
         dispatch(loadAdmin());
       })
       .catch((error) => {
@@ -72,8 +73,11 @@ const AdminSettings = () => {
   };
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center">
-      <div className="flex w-full 800px:w-[80%] flex-col justify-center my-5">
+    <div
+      className="w-full min-h-screen flex flex-col items-center mt-3"
+      style={{ marginBottom: "100px" }}
+    >
+      <div className="p-4 flex w-full bg-white lg:w-[50%] flex-col justify-center my-5 bg-[#171203] rounded-lg shadow-lg hover:shadow-xl hover:border-white border border-transparent transition duration-300">
         <div className="w-full flex items-center justify-center">
           <div className="relative">
             <img
@@ -97,86 +101,218 @@ const AdminSettings = () => {
 
         {/* shop info */}
         <form className="flex flex-col items-center" onSubmit={updateHandler}>
-          <div className="w-[100%] flex items-center flex-col 800px:w-[50%] mt-5">
+          {/* <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-5">
             <div className="w-full pl-[3%]">
-              <label className="block pb-2">Shop Name</label>
+              <label className="block pb-2 font-[800] text-black">
+                Shop Name
+              </label>
             </div>
             <input
               type="name"
               placeholder={`${admin.name}`}
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+              className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text-gray-600`}
               required
             />
-          </div>
-          <div className="w-[100%] flex items-center flex-col 800px:w-[50%] mt-5">
+          </div> */}
+
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
             <div className="w-full pl-[3%]">
-              <label className="block pb-2">Shop description</label>
+              <label
+                className="block pb-2 font-[600] text-black"
+                style={{ fontFamily: "Roboto, sans-serif" }}
+              >
+                Shop Name
+              </label>
+            </div>
+            <div class="relative h-11 w-[350px] min-w-[200px]">
+              <input
+                type="name"
+                placeholder={`${admin.name}`}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+            </div>
+          </div>
+
+          {/* <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
+            <div className="w-full pl-[3%]">
+              <label className="block pb-2 font-[800] text-black">
+                Shop description
+              </label>
             </div>
             <input
               type="name"
               placeholder={`${
                 admin?.description
                   ? admin.description
-                  : 'Enter your shop description'
+                  : "Enter your shop description"
               }`}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+              className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text`}
             />
-          </div>
-          <div className="w-[100%] flex items-center flex-col 800px:w-[50%] mt-5">
+          </div> */}
+
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
             <div className="w-full pl-[3%]">
-              <label className="block pb-2">Shop Address</label>
+              <label
+                className="block pb-2 font-[600] text-black"
+                style={{ fontFamily: "Roboto, sans-serif" }}
+              >
+                Shop description
+              </label>
+            </div>
+            <div class="relative h-11 w-[350px] min-w-[200px]">
+              <input
+                type="name"
+                placeholder={`${
+                  admin?.description
+                    ? admin.description
+                    : "Enter your shop description"
+                }`}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+            </div>
+          </div>
+          {/* 
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
+            <div className="w-full pl-[3%]">
+              <label className="block pb-2 font-[800] text-black">
+                Shop Address
+              </label>
             </div>
             <input
               type="name"
               placeholder={admin?.address}
               value={address}
               onChange={(e) => setAddress(e.target.value)}
-              className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+              className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text-gray-600`}
               required
             />
-          </div>
+          </div> */}
 
-          <div className="w-[100%] flex items-center flex-col 800px:w-[50%] mt-5">
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
             <div className="w-full pl-[3%]">
-              <label className="block pb-2">Shop Phone Number</label>
+              <label
+                className="block pb-2 font-[600] text-black"
+                style={{ fontFamily: "Roboto, sans-serif" }}
+              >
+                Shop Address
+              </label>
+            </div>
+            <div class="relative h-11 w-[350px] min-w-[200px]">
+              <input
+                type="name"
+                placeholder={admin?.address}
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+                className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+            </div>
+          </div>
+          {/* 
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
+            <div className="w-full pl-[3%]">
+              <label className="block pb-2 font-[800] text-black">
+                Shop Phone Number
+              </label>
             </div>
             <input
               type="number"
               placeholder={admin?.phoneNumber}
               value={phoneNumber}
               onChange={(e) => setPhoneNumber(e.target.value)}
-              className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+              className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text-gray-600`}
               required
             />
+          </div> */}
+
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
+            <div className="w-full pl-[3%]">
+              <label
+                className="block pb-2 font-[600] text-black"
+                style={{ fontFamily: "Roboto, sans-serif" }}
+              >
+                Shop Phone Number
+              </label>
+            </div>
+            <div class="relative h-11 w-[350px] min-w-[200px]">
+              <input
+                type="number"
+                placeholder={admin?.phoneNumber}
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+                className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+            </div>
           </div>
 
-          <div className="w-[100%] flex items-center flex-col 800px:w-[50%] mt-5">
+          {/* <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
             <div className="w-full pl-[3%]">
-              <label className="block pb-2">Shop Zip Code</label>
+              <label className="block pb-2 font-[800] text-black">
+                Shop Zip Code
+              </label>
             </div>
             <input
               type="number"
               placeholder={admin?.zipCode}
               value={zipCode}
               onChange={(e) => setZipcode(e.target.value)}
-              className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+              className={`${styles.input} !w-[95%] mb-4 800px:mb-0 text-gray-600`}
               required
             />
+          </div> */}
+
+          <div className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-2">
+            <div className="w-full pl-[3%]">
+              <label
+                className="block pb-2 font-[600] text-black"
+                style={{ fontFamily: "Roboto, sans-serif" }}
+              >
+                Shop Zip Code
+              </label>
+            </div>
+            <div class="relative h-11 w-[350px] min-w-[200px]">
+              <input
+                type="number"
+                placeholder={admin?.zipCode}
+                value={zipCode}
+                onChange={(e) => setZipcode(e.target.value)}
+                required
+                className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+            </div>
           </div>
 
-          <div className="w-[100%] flex items-center flex-col 800px:w-[50%] mt-5">
-            <input
+          <div
+            className="w-[100%] flex items-center flex-col 800px:w-[80%] mt-5"
+            style={{ marginBottom: "20px" }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+              required
+              //   readOnly
+            >
+              Update Shop
+            </Button>
+          </div>
+          {/* <input
               type="submit"
               value="Update Shop"
               className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
               required
-              readOnly
-            />
-          </div>
+            /> */}
         </form>
       </div>
     </div>
--- frontend/src/components/Admin/AllUsers.jsx
+++ frontend/src/components/Admin/AllUsers.jsx
@@ -1,42 +1,65 @@
-import Button from '@mui/material/Button';
-import { DataGrid } from '@mui/x-data-grid';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Loader from '../Layout/Loader';
-import { AiOutlineArrowRight } from 'react-icons/ai';
-import { getAllUsers } from '../../redux/action/user';
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import { DataGrid } from "@mui/x-data-grid";
+import { useDispatch, useSelector } from "react-redux";
+import { AiOutlineDelete, AiOutlineSearch } from "react-icons/ai";
+import Loader from "../Layout/Loader";
+import { getAllUsers, deleteUser } from "../../redux/action/user";
 
 const AllUsers = () => {
-  const {usersList, isLoading} = useSelector((state) => state.user);
+  const { usersList, isLoading } = useSelector((state) => state.user);
   const { admin } = useSelector((state) => state.admin);
-  console.log("id from component:", admin._id);
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const dispatch = useDispatch()
-
-  useEffect(()=>{
-      dispatch(getAllUsers(admin._id));
+  useEffect(() => {
+    dispatch(getAllUsers(admin._id));
   }, [dispatch, admin._id]);
-  
-  console.log("list of users are: ", usersList);
+
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete this user?")) {
+      dispatch(deleteUser(id));
+    }
+  };
+
+  const filteredUsers = usersList.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   const columns = [
-  { field: 'id', headerName: 'User ID', minWidth: 150, flex: 0.7 },
-  { field: 'name', headerName: 'User Name', minWidth: 150, flex: 0.7 },
-  { field: 'email', headerName: 'User Email', minWidth: 130, flex: 0.8 },
-  {
-    field: 'phonenumber',
-    headerName: 'User Phone Number',
-    type: 'string',
-    minWidth: 150,
-    flex: 0.8,
-  },
+    { field: "id", headerName: "User ID", minWidth: 100, flex: 1 },
+    { field: "name", headerName: "User Name", minWidth: 100, flex: 1 },
+    { field: "email", headerName: "User Email", minWidth: 150, flex: 1 },
+    {
+      field: "phonenumber",
+      headerName: "User Phone Number",
+      type: "string",
+      minWidth: 150,
+      flex: 1,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      minWidth: 130,
+      flex: 0.5,
+      renderCell: (params) => (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleDelete(params.row.id)}
+        >
+          <AiOutlineDelete size={20} />
+          Delete
+        </Button>
+      ),
+    },
   ];
 
-  const rows = usersList.map((user) => ({
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      phonenumber: user.phoneNumber,
+  const rows = filteredUsers.map((user) => ({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    phonenumber: user.phoneNumber,
   }));
 
   return (
@@ -44,18 +67,40 @@ const AllUsers = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="w-full mx-8 pt-1 mt-10 bg-white">
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={10}
-            disableSelectionOnClick
-            autoHeight
-          />
+        <div
+          className="w-full px-0 sm:px-8 pt-1 mt-10 bg-white overflow-x-auto"
+          style={{ marginBottom: "60px", borderRadius: "10px" }}
+        >
+          <div className="mx-4">
+            <div className="w-full flex justify-end items-center mb-4">
+              <div className="relative w-full sm:w-[40%] mt-5">
+                <input
+                  type="text"
+                  placeholder="Search Users..."
+                  className="h-10 sm:h-[45px] pl-4 pr-10 w-full  border-2 border-solid border-[#ff9800] rounded-md placeholder-[#9e8a4f] shadow-md transition ease-in-out duration-300  focus:outline-none hover:border-[#ff9800] hover:ring-[#ff9800] hover:ring-2"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <AiOutlineSearch
+                  size={30}
+                  className="absolute right-2 top-1.5 cursor-pointer"
+                />
+              </div>
+            </div>
+            <div className="w-full overflow-x-auto mb-10">
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                disableSelectionOnClick
+                autoHeight
+                className="data-grid"
+              />
+            </div>
+          </div>
         </div>
-      )} 
+      )}
     </>
-
   );
 };
 
